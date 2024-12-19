7f4d0cb36f4e812b55294590955af9386ff602ef
evanwu03 <ewu43951@gmail.com>
2024-12-19 14:03:37 -0600

    Simulated a box falling midair to ground

--- src/domino.c
+++ src/domino.c
@@ -16,12 +16,15 @@ const static int SUB_STEPS = 4;              ///< Box2D simulation substeps
 const static float SCALED_WIDTH = WINDOW_WIDTH * SCALE;
 const static float SCALED_HEIGHT = WINDOW_HEIGHT * SCALE;
 
-static SDL_Window *window = NULL;     ///< The main SDL window
-static SDL_Renderer *renderer = NULL; ///< The main SDL renderer
+static SDL_Window *window;     ///< The main SDL window
+static SDL_Renderer *renderer; ///< The main SDL renderer
 
 static b2WorldId worldId; ///< The Box2D world to use for simulation
 static b2BodyId groundId; ///< The static ground body
 static b2BodyId bodyId;   ///< Dynamic body
+
+// Function prototypes
+SDL_FRect box2DtoSDL(b2BodyId bodyId);
 
 int main(int argc, char *argv[])
 {
@@ -53,6 +56,18 @@ int main(int argc, char *argv[])
     const b2ShapeDef groundShapeDef = b2DefaultShapeDef();
     b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
 
+     // create the falling box
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = (b2Vec2){0.0f, 100.0f};
+    bodyId = b2CreateBody(worldId, &bodyDef);
+    b2Polygon dynamicBox = b2MakeBox(10.0f, 10.0f);
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.density = 1.0f;
+    shapeDef.friction = 0.3f;
+    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+
+
     bool isRunning = true;
     // Main loop where rendering happens
     while (isRunning)
@@ -72,10 +87,62 @@ int main(int argc, char *argv[])
             default:
             }
         }
+
+        // run the simulation
+        b2World_Step(worldId, TIME_STEP, SUB_STEPS);
+
+        // clear screen
+        SDL_SetRenderDrawColor(renderer, 25, 25, 25, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+
+        // render the ground
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_FRect rect = box2DtoSDL(groundId);
+        SDL_RenderFillRect(renderer, &rect);
+
+        // render the falling cube
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+        rect = box2DtoSDL(bodyId);
+        SDL_RenderFillRect(renderer, &rect);
+
+
+        SDL_RenderPresent(renderer);
     }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     b2DestroyWorld(worldId);
 
     return 0;
+}
+
+/**
+ * Convert the given box2D body's coordinates from box2D to SDL screen-space. For the sake of simplicity, this function
+ * assumes that the body has a rectangular collision shape.
+ * @param bodyId The ID of the Box2D body to convert.
+ * @return The new SDL rectangle.
+ */
+SDL_FRect box2DtoSDL(b2BodyId bodyId)
+{
+    b2Vec2 position = b2Body_GetPosition(bodyId);
+
+    // get the body's collision polygon
+    b2ShapeId shapes[1];
+    b2Body_GetShapes(bodyId, shapes, 1);
+    const b2Polygon polygon = b2Shape_GetPolygon(shapes[0]);
+
+    // create the SDL rectangle
+    SDL_FRect rect;
+
+    // calculate the object's width and height
+    rect.w = fabsf(polygon.vertices[0].x - polygon.vertices[1].x) * SCALE;
+    rect.h = fabsf(polygon.vertices[1].y - polygon.vertices[2].y) * SCALE;
+
+    printf("%f\n", rect.h);
+
+    // calculate the object's X and Y positions in screen space
+    rect.x = (SCALED_WIDTH / 2.0f + position.x) * SCALE - rect.w / 2;
+    rect.y = (SCALED_HEIGHT / 2.0f - position.y) * SCALE - rect.h / 2;
+
+    return rect;
 }
